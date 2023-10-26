ea5f934666c3cac32a2eaad8f378c1662a979aed
Miguel Melluzzi <110363714+MelluzziMiguel@users.noreply.github.com>
2023-10-26 15:10:59 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# projectPHP
-Repositório de Estudo
+# sql_3
+Conteudo de SQL do 3º Trimestre - CEEP Cianorte
+
+50 pontos -> projeto em sala de aula - C.E.R.A
--- index.php
+++ index.php
@@ -5,9 +5,10 @@ com o banco
 de dados*/
 $servername = "localhost";
 //você deu nome ao banco de dados
-$database = "func2c";
+$database = "func2c"; //func2c ou func2d
 $username = "root";
 $password = "";
+
 
 $conexao = mysqli_connect(
     $servername, $username, 
@@ -17,37 +18,61 @@ $conexao = mysqli_connect(
 if (!$conexao){
     die("Falha na conexão".mysqli_connect_error());
 }
-// echo "E essa palheta ai bb ;)  :P";
-// echo  $id." ".$nome." ".$cpf." ".$botao;
+//echo "conectado com sucesso";
 
 $id = $_POST["id"];
 $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];
 $botao = $_POST["botao"];
 
+if(empty($botao)){
 
-$sql = "INSERT INTO funcionarios (id, nome, cpf) 
-        VALUES ('', '$nome', '$cpf')";
+}else if($botao == "Cadastrar"){
+    $sql = "INSERT INTO funcionarios 
+    (id, nome, cpf) VALUES('','$nome', '$cpf')";
+}
 
-if($botao == "Cadastrar"){
-    if (mysqli_query($conexao, $sql)){
-        echo "Cadastrado com sucesso!";
+//aqui vou tratar erros nas operações C.E.R.A
+if(!empty($sql)){
+    if(mysqli_query($conexao, $sql)){
+        echo "Operação realizada com sucesso";
     }else{
-        echo "Falha ao cadastrar!";
+        echo "Houve um erro na operação: <br />";
+        echo  mysqli_error($conexao);
     }
 }
 
+$selecionado = $_GET["id"];
 
+if(!empty($selecionado)){
+    $sql_selecionado = "SELECT * FROM funcionarios WHERE id = $selecionado";
+
+    $resultado = mysqli_query($conexao, $sql_selecionado);
+
+    while($linha = mysqli_fetch_assoc($resultado)){
+        $id = $linha["id"];
+        $nome = $linha["nome"];
+        $cpf = $linha["cpf"];
+    }
+}
+
+//echo $id." ".$nome." ".$cpf." ".$botao;
 
 ?>
 
 <html>
     <head>
         <style>
+            body {
+                background color: #AAAAAA;
+            }
+
 
             form {
                 display: flex;
                 flex-direction: column;
+                text-align: center;    
+                align-items: center;
             }
 
             input {
@@ -64,20 +89,44 @@ if($botao == "Cadastrar"){
             .buttons input {
                 width: 80px;
             }
+
+            .informacoes {
+                color = black;
+            }
         </style>
     </head>
     <body>
         <form name="func" method = "POST">
             <label for="id">ID:</label> 
-            <input type="text" name = "id">
+            <input type="text" name = "id" value = "<?php echo $id; ?>">
             <label for="nome">NOME:</label> 
-            <input type="text" name = "nome">
+            <input type="text" name = "nome" value = "<?php echo $nome; ?>">
             <label for="cpf">CPF:</label> 
-            <input type="text" name = "cpf">
+            <input type="text" name = "cpf" value = "<?php echo $cpf; ?>">
                 <div class="buttons">
                     <input type="submit" name = "botao" value="Cadastrar">
                     <input type="reset" name = "botao" value="Cancelar">
                 </div>
         </form>
+        <table>
+            
+
+            <?php
+            $sql_mostra_cad = "SELECT * FROM funcionarios ORDER BY id desc limit 0,10";
+            $resultado = mysqli_query($conexao, $sql_mostra_cad);
+
+            while($linha = mysqli_fetch_assoc($resultado)){
+                echo "
+                <tr>
+                     <td>
+                     <a href='?id=".$linha["id"]."'>Selecionar</a>
+                     </td>
+                     <td>".$linha["id"]."</td>
+                     <td>".$linha["nome"]."</td>
+                     <td>".$linha["cpf"]."</td>
+                </tr>";
+            }
+            ?>
+        </table>
     </body>
 </html>
